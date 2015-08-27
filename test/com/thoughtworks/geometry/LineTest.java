0cbc0a50111fcba5027d2aa804875073554bf900
srikantk <kaanth.katta@gmail.com>
2015-08-27 19:54:12 +0530

    wrting another spec for points which lies on diagonal axis

--- test/com/thoughtworks/geometry/LineTest.java
+++ test/com/thoughtworks/geometry/LineTest.java
@@ -31,6 +31,12 @@ public class LineTest {
         assertEquals(1.414213, line.length(), 0.2d);
     }
 
+    @Test
+    public void havingBothStartPointsAndEndPointsOnCurve() {
+        Line line = new Line(1.0, 1.0, 3.0, 2.0);
+        assertEquals(2.236, line.length(), 0.2d);
+    }
+
 
 
 }
