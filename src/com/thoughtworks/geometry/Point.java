0bff29708ab1958b6005ca2ccac6a97c4ae2cd68
srikantk <kaanth.katta@gmail.com>
2015-09-03 10:38:04 +0530

    "chage the prime in hash code"

--- src/com/thoughtworks/geometry/Point.java
+++ src/com/thoughtworks/geometry/Point.java
@@ -35,7 +35,7 @@ public class Point {
 
     @Override
     public int hashCode() {
-        double hash = 79;
+        double hash = 81;
         hash = hash * 31 + this.x;
         hash = hash * 31 + this.y;
         return (int) hash;
